3d8604a959fea1f3f402cf9ef02690e2036ca563
shijian <jian.shi@ys100.com>
2025-11-29 11:10:13 +0800

    1

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,8 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
-import { type api } from './index'
+
 declare global {
   interface Window {
     electron: ElectronAPI
-    api: typeof api
+    api: unknown
   }
 }
