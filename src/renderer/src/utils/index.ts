30d36652aca68584fb1eba4d1bf5938f4df984a8
shijian <jian.shi@ys100.com>
2025-12-04 20:56:38 +0800

    1

--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -1,10 +1,12 @@
-
 export const copyText = (text: string) => {
   if (text) {
-    navigator.clipboard.writeText(text).then(() => {
-      console.log('消息已复制到剪贴板');
-    }).catch(err => {
-      console.error('复制失败:', err);
-    });
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        console.log('消息已复制到剪贴板')
+      })
+      .catch((err) => {
+        console.error('复制失败:', err)
+      })
   }
-};
+}
