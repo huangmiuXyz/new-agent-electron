02fce60a45c37f2ceccd99350058f573e21222b0
shijian <jian.shi@ys100.com>
2025-12-13 21:57:29 +0800

    11

--- src/renderer/src/composables/useUpload.ts
+++ src/renderer/src/composables/useUpload.ts
@@ -173,7 +173,6 @@ export function useUpload(options: UseUploadOptions = {}) {
 
       if (fileHandles && fileHandles.length > 0) {
         await processFileSystemHandles(fileHandles)
-
         if (shouldSaveFileToUserData) {
           const files = await Promise.all(
             selectedFiles.value.map(async (f) => ({
