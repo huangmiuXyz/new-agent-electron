9bcb956a9968b9fabf69121b9dc9c0e7df503c39
shijian <jian.shi@ys100.com>
2025-12-19 13:53:21 +0800

    1

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -36,9 +36,6 @@ export const api = {
 }
 
 export type API = typeof api
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
