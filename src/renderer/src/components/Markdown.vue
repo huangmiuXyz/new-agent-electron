a73cb44028ef9c243046ece2ea9280f26c90079f
shijian <jian.shi@ys100.com>
2025-12-31 15:55:15 +0800

    1

--- src/renderer/src/components/Markdown.vue
+++ src/renderer/src/components/Markdown.vue
@@ -19,26 +19,38 @@ const { display } = storeToRefs(settingsStore)
 const incremark = useIncremark({
   gfm: true
 })
+
+const lastTextLength = ref(0)
+
+const updateMarkdown = (newText: string) => {
+  if (newText.length < lastTextLength.value) {
+    incremark.reset()
+    lastTextLength.value = 0
+  }
+  const chunk = newText.slice(lastTextLength.value)
+  if (chunk) {
+    incremark.append(chunk)
+    lastTextLength.value = newText.length
+  }
+}
+
 watch(
   () => props.message,
   () => {
-    if (props.block.state === 'streaming') {
-      incremark.render(props.block.text)
-    }
     if (props.block.state === 'done') {
       incremark.finalize()
+    } else {
+      updateMarkdown(props.block.text)
     }
   },
-  {
-    deep: true
-  }
+  { immediate: true, deep: true }
 )
 watch(() => display.value.darkMode, () => {
   incremarkTheme.value = display.value.darkMode ? 'dark' : 'default'
 })
+
 onMounted(() => {
   incremarkTheme.value = display.value.darkMode ? 'dark' : 'default'
-  incremark.render(props.block.text)
 })
 </script>
 <style>
