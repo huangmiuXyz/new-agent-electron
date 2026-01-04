cfcfd9464b9730249d9e53ac8fc0bd0c7ff5eaac
shijian <jian.shi@ys100.com>
2026-01-04 13:36:43 +0800

    1

--- packages/qi-cli/src/commands/build.ts
+++ packages/qi-cli/src/commands/build.ts
@@ -62,7 +62,10 @@ export const buildCommand = new Command('build')
       // 添加更新时间
       const now = new Date();
       info.updatedAt = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
+
       const updatedInfoContent = JSON.stringify(info, null, 2);
+
+      await fs.writeFile(infoJsonPath, updatedInfoContent);
 
       // 检查 dist 目录是否存在
       const distDir = path.join(path.dirname(infoJsonPath), 'dist');
