dd9a2be1f7e548e71a20a90ff5e3ed0746543d3a
shijian <jian.shi@ys100.com>
2026-01-06 09:39:16 +0800

    1

--- src/renderer/src/composables/useUpload.ts
+++ src/renderer/src/composables/useUpload.ts
@@ -79,7 +79,7 @@ export function useUpload(options: UseUploadOptions = {}) {
             mediaType: window.api.mime.lookup(path) as string,
             blobUrl: URL.createObjectURL(blob),
             filename: window.api.path.basename(path),
-            path: path,
+            path: 'file://' + filePaths,
             name: window.api.path.basename(path),
             type: 'file' as const,
             size: blob.size
