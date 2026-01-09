23256833cd1abdbfd0e7eab8fd4afd116a869554
shijian <jian.shi@ys100.com>
2026-01-09 15:31:56 +0800

    1

--- src/renderer/src/services/chatService/minimax/index.ts
+++ src/renderer/src/services/chatService/minimax/index.ts
@@ -1,3 +1,2 @@
 export { createMiniMax, minimax } from './minimax-provider';
 export type { MiniMaxProvider, MiniMaxProviderSettings } from './minimax-provider';
-export { VERSION } from './version';
