6494c48cbff3785fcb859a272eba602c10242813
二五六仔 <534496845@qq.com>
2026-02-16 18:24:37 +0800

    1

--- check_lowlight.ts
+++
@@ -1,6 +0,0 @@
-
-import { common } from 'lowlight';
-
-console.log('Keys in common:', Object.keys(common).sort());
-console.log('Is html in common?', 'html' in common);
-console.log('Is xml in common?', 'xml' in common);
